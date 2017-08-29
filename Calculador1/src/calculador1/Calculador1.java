c5c3dbd3e55ece0e90c510dbc2260771c5df1958
Estudiantes <Estudiantes@Labing70210.udistrital.edu.co>
2017-08-29 06:15:43 -0500

    .

---
+++ Calculador1/src/calculador1/Calculador1.java
@@ -0,0 +1,41 @@
+package calculador1;
+
+public class Calculador1 {
+
+    float operando1;
+    float operando2;
+    float resultado;
+
+    public static void main(String[] args) {
+
+    }
+
+    void suma() {
+        resultado = operando1 + operando2;
+
+    }
+        void resta() {
+        resultado = operando1 - operando2;
+
+    }
+            void multi() {
+        resultado = operando1 * operando2;
+
+    }
+                void divi() {
+        resultado = operando1 / operando2;
+
+    }
+                    void facto() {
+        resultado = operando1 % operando2;
+
+    }
+                    
+
+    static class micalculadora {
+
+        public micalculadora() {
+        }
+    }
+
+}
